9c48cc1f86f9d405a9bccb20078f0713ba508772
xGuiilherme <guiroox2@gmail.com>
2022-01-04 17:22:44 -0300

    Create SecurityConfig for Authentication in Memory!

---
+++ src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package academy.devdojo.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Todas as requisição que fizer(das url que está no controller) vão precisar passar por uma autenticação básica.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    // Criptografa a senha: vai codifica a palavra 'test'
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoder {}", passwordEncoder.encode("test"));
+
+        // Cria um Usuario e Password em memória e vai ficar ativo durante o ciclo de vida da aplicação até reiniciar
+        // Quando reiniciado vai criar novamente com a mesma senha e mesmo usuario.
+        auth.inMemoryAuthentication()
+                .withUser("Guilherme")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
+}
