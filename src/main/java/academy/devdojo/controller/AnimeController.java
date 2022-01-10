aa0d1d65467b5f50ac63d501c071c076b25622b7
xGuiilherme <guiroox2@gmail.com>
2022-01-10 17:36:05 -0300

    Created Documentation with SpringDoc OpenAPI - Swagger

--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -4,13 +4,16 @@ import academy.devdojo.domain.Anime;
 import academy.devdojo.requests.AnimePostRequestBody;
 import academy.devdojo.requests.AnimePutRequestBody;
 import academy.devdojo.service.AnimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -27,18 +30,22 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(Pageable pageable) {
+    @Operation(summary = "List all animes paginated", description = "The default size is 20, use the parameter size to change the default value",
+    tags = {"anime"})
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
     // Novo end-point onde não é paginavel
     @GetMapping(path = "/all")
+    @Operation(summary = "List all animes", description = "Returns a list of all saved anime")
     public ResponseEntity<List<Anime>> listAll() {
         return ResponseEntity.ok(animeService.listAllNoPageable());
     }
 
     // Os dados são enviados como String anexada a URL 'são visíveis ao usuário'
     @GetMapping(path = "/{id}")
+    @Operation(summary = "Search by id", description = "Returns by id of anime")
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         return ResponseEntity.ok(animeService.findByIdOrdThrowBadRequestException(id));
     }
@@ -51,6 +58,7 @@ public class AnimeController {
     }
 
     @GetMapping(path = "/find")
+    @Operation(summary = "Search by name", description = "Returns by name of anime")
     public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
         return ResponseEntity.ok(animeService.findByName(name));
     }
@@ -62,6 +70,10 @@ public class AnimeController {
     }
 
     @DeleteMapping(path = "/admin/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "When Anime Does Not Exist in The Database"),
+    })
     public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -69,6 +81,7 @@ public class AnimeController {
 
     // Quando executado ele está dando um 'replace' substituindo o estado inteiro do objeto.
     @PutMapping
+    @Operation(summary = "Replace an resource", description = "Create a new resource or replaces already existing resource")
     public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
         animeService.replace(animePutRequestBody);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/academy/devdojo/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/requests/AnimePostRequestBody.java
@@ -1,5 +1,6 @@
 package academy.devdojo.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,6 @@ public class AnimePostRequestBody {
 
     //Faz a validação dos campos diretamente no Controller sem precisar do If
     @NotEmpty(message = "The anime cannot be empty")
+    @Schema(description = "This is the Anime's name", example = "Tensei Shitara Slime Datta Ken", required = true)
     private String name;
 }
